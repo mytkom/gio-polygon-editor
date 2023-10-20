cdf18a323e7205c4ed9557a11b1aac499d0d3902
Marek Mytkowski <marek.mytkowski.mm@gmail.com>
2023-10-20 14:14:23 +0200

    Testing, moving vertices and try to move edge

--- polygon_builder.go
+++ polygon_builder.go
@@ -4,84 +4,153 @@ import (
 	"image/color"
 
 	"gioui.org/f32"
+	"gioui.org/io/pointer"
+	"gioui.org/layout"
 	"gioui.org/op"
 	"gioui.org/op/clip"
 	"gioui.org/op/paint"
 )
 
 type PolygonBuilder struct {
-    vertices []Vertex
-    active bool
+    vertices []*Vertex
     PointerTailEndPosition f32.Point
+    vertexAddEventTag bool
+    Color color.NRGBA
 }
 
-func (pb *PolygonBuilder) Layout(ops *op.Ops) {
+func (pb *PolygonBuilder) Layout(gtx *layout.Context) {
     if len(pb.vertices) == 0 {
         return
     }
 
-    for _, vertex := range pb.vertices {
-        vertex.Layout(ops)
-    }
+    drawPathFromVertices(pb.vertices, gtx.Ops, &pb.Color)
 
-    lastVertex := pb.vertices[0]
-    var path clip.Path 
-
-    for i := 1; i < len(pb.vertices); i++ {
-        path.Begin(ops)
-        path.MoveTo(lastVertex.Point)
-        path.LineTo(pb.vertices[i].Point)
-        path.Close()
-
-        paint.FillShape(ops, color.NRGBA{R:255, A:255},
-        clip.Stroke{
-            Path: path.End(),
-            Width: 4,
-        }.Op()) 
-
-        lastVertex = pb.vertices[i]
-    }
-
-    pb.drawCursorTailLine(ops)
+    pb.drawCursorTailLine(gtx.Ops)
 }
 
-func (pb *PolygonBuilder) drawCursorTailLine(ops *op.Ops) {
-    if !pb.active {
+func (pb *PolygonBuilder) HandleEvents(gtx *layout.Context) {
+    if len(pb.vertices) > 2 {
+        pb.handleClosingVertexEvent(gtx) 
+    }
+    pb.handleAddVertexEvent(gtx)
+}
+
+func (pb *PolygonBuilder) handleAddVertexEvent(gtx *layout.Context) {
+    if eventsStoppedInFrame {
+        return
+    } 
+
+    for _, ev := range gtx.Events(&pb.vertexAddEventTag) {
+        if x, ok := ev.(pointer.Event); ok {
+            switch x.Type {
+            case pointer.Press:
+                polygonBuilder.addVertex(x.Position)
+            case pointer.Move:
+                polygonBuilder.setTailEnd(x.Position)
+            }
+        }
+    }
+}
+
+func (pb *PolygonBuilder) handleClosingVertexEvent(gtx *layout.Context) {
+    if eventsStoppedInFrame {
         return
     }
 
+    v := pb.vertices[0]
+
+    for _, ev := range gtx.Events(&v.Hovered) {
+        if x, ok := ev.(pointer.Event); ok {
+            switch x.Type {
+            case pointer.Press:
+                CreatePolygon(pb.vertices, color.NRGBA{R: 255, G: 255, B: 255, A: 255}) 
+                pb.vertices = []*Vertex{}
+                StopEventsBelow() 
+            case pointer.Enter:
+                pb.vertices[0].Hovered = true 
+            case pointer.Leave:
+                pb.vertices[0].Hovered = false
+            }
+        }
+    }
+}
+
+func (pb *PolygonBuilder) RegisterEvents(gtx *layout.Context) {
+    pb.registerAddVertexEvent(gtx)
+    if len(pb.vertices) > 2 {
+        pb.registerClosingVertexEvent(gtx)
+    }
+}
+
+func (pb *PolygonBuilder) registerAddVertexEvent(gtx *layout.Context) {
+    pointer.InputOp{
+        Tag: &pb.vertexAddEventTag,
+        Types: pointer.Press | pointer.Release | pointer.Move,
+    }.Add(gtx.Ops)
+}
+
+func (pb *PolygonBuilder) registerClosingVertexEvent(gtx *layout.Context) {
+    v := pb.vertices[0]
+
+    defer v.GetHoverEllipse().Push(gtx.Ops).Pop()  
+    pointer.InputOp{
+        Tag: &v.Hovered,
+        Types: pointer.Press | pointer.Enter | pointer.Leave,
+    }.Add(gtx.Ops)
+}
+
+func (pb *PolygonBuilder) drawCursorTailLine(ops *op.Ops) {
     var path clip.Path 
     path.Begin(ops)
     path.MoveTo(pb.vertices[len(pb.vertices)-1].Point)
     path.LineTo(pb.PointerTailEndPosition)
     path.Close()
 
-    paint.FillShape(ops, color.NRGBA{R:255, A:255},
+    paint.FillShape(ops, pb.Color,
     clip.Stroke{
         Path: path.End(),
-        Width: 4,
+        Width: float32(lineWidth),
     }.Op())
 }
 
-func (pb *PolygonBuilder) SetTailEnd(newTailEndPosition f32.Point) {
+func (pb *PolygonBuilder) setTailEnd(newTailEndPosition f32.Point) {
    pb.PointerTailEndPosition = newTailEndPosition
 }
 
-func (pb *PolygonBuilder) AddVertex(pointerPosition f32.Point) {
-    vertexDefaultColor := color.NRGBA{R: 255, A: 255}
-
-    if len(pb.vertices) == 0 {
-        pb.active = true
-    }
-
+func (pb *PolygonBuilder) addVertex(pointerPosition f32.Point) {
     vertex := &Vertex{
         Point: pointerPosition,
-        Color: vertexDefaultColor,
     }
 
     pb.vertices = append(
         pb.vertices, 
-        *vertex,
+        vertex,
     )
-    pb.SetTailEnd(pointerPosition)
+    pb.setTailEnd(pointerPosition)
+}
+
+func drawPathFromVertices(v []*Vertex, ops *op.Ops, color *color.NRGBA) {
+    path := getPathFromVertices(v, ops, *color)
+
+    paint.FillShape(ops, *color,
+    clip.Stroke{
+        Path: path.End(),
+        Width: float32(lineWidth),
+    }.Op()) 
+}
+
+func getPathFromVertices(v []*Vertex, ops *op.Ops, color color.NRGBA) clip.Path {
+    var path clip.Path
+
+    for _, vertex := range v {
+        vertex.Layout(ops, color)
+    }
+
+    path.Begin(ops)
+    path.MoveTo(v[0].Point)
+    for i := 1; i < len(v); i += 1 {
+        path.LineTo(v[i].Point)
+    }
+
+    return path
 }
--- vertex.go
+++ vertex.go
@@ -11,17 +11,38 @@ import (
 )
 
 const vertexRadius = 5
+const vertexHoverRadius = vertexRadius + 10
 
 type Vertex struct {
     Point f32.Point
-    Color color.NRGBA
+    Hovered bool
+    Selected bool
 }
 
-func (v *Vertex) Layout(ops *op.Ops) {
-    imagePoint := v.Point.Round()
-    circle := clip.Ellipse{
-        Min: image.Pt(imagePoint.X - vertexRadius, imagePoint.Y - vertexRadius),
-        Max: image.Pt(imagePoint.X + vertexRadius, imagePoint.Y + vertexRadius),
-    }.Op(ops)
-    paint.FillShape(ops, v.Color, circle)
+func (v *Vertex) Layout(ops *op.Ops, c color.NRGBA) {
+    circle := v.GetEllipse().Op(ops)
+    paint.FillShape(ops, c, circle)
+
+    if v.Hovered || v.Selected {
+        circle = v.GetHoverEllipse().Op(ops)
+        paint.FillShape(ops, HoverizeColor(c), circle)
+    }
 }
+
+func (v* Vertex) GetEllipse() clip.Ellipse {
+    return v.getClipEllipse(vertexRadius)
+}
+
+func (v* Vertex) GetHoverEllipse() clip.Ellipse {
+    return v.getClipEllipse(vertexHoverRadius)
+}
+
+func (v *Vertex) getClipEllipse(radius int) clip.Ellipse {
+    imagePoint := v.Point.Round()
+
+    return clip.Ellipse{
+        Min: image.Pt(imagePoint.X - radius, imagePoint.Y - radius),
+        Max: image.Pt(imagePoint.X + radius, imagePoint.Y + radius),
+    }   
+}
+
